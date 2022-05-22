e56fb0d6695fde8a89e1fd3f855f02a486cbcdb3
sunn <tjsntjsn20@gmail.com>
2022-05-22 14:00:38 +0900

    2022-05-22 : change blog-detail pages by FBV

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index),
-    path('/<int:pk>/', views.single_post_page),
+    path('/', views.PostList.as_view()),
+    path('/<int:pk>/', views.PostDetail.as_view())
+    # path('', views.index),
+    # path('/<int:pk>/', views.single_post_page),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,36 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
-def index(request):
-    posts =  Post.objects.all().order_by('-pk')
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+    # template_name = 'blog/index.html'
+
+# def index(request):
+#     posts =  Post.objects.all().order_by('-pk')
     
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-        }
-    )
+#     return render(
+#         request,
+#         'blog/index.html',
+#         {
+#             'posts': posts,
+#         }
+#     )
 
 
-def single_post_page(request, pk: int):
-    post = Post.objects.get(pk=pk)
+class PostDetail(DetailView):
+    model = Post 
+    # template_name = 'blog/single_post_page.html'
     
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post': post
-        }
-    )
+# def single_post_page(request, pk: int):
+#     post = Post.objects.get(pk=pk)
+    
+#     return render(
+#         request,
+#         'blog/single_post_page.html',
+#         {
+#             'post': post
+#         }
+#     )
