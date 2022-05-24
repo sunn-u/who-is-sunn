f85fb145295b3b0b6366fa15296311b62edf69b0
sunn <tjsntjsn20@gmail.com>
2022-05-24 23:39:32 +0900

    2022-05-24 : add download-file-button on blog-page

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+import os
 from django.db import models
 
 
@@ -15,4 +16,9 @@ class Post(models.Model):
     
     def get_absolute_url(self):
         return f"/blog/{self.pk}"
- 
+    
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+    
+    def get_file_ext(self):
+        return self.get_file_name.split('.')[-1]
